e8fcfee94669e4674c290c6a5d872707d1a61420
adervark <bhavishnithinr.work@gmail.com>
2026-03-24 11:19:23 +0530

    Culmination

--- install.bat
+++ install.bat
@@ -35,11 +35,11 @@ echo.
 echo [2/4] Installing PyTorch with CUDA 12.4 support...
 echo This may take a few minutes as PyTorch is a large download.
 call .venv\Scripts\activate.bat
-uv pip install torch --extra-index-url https://download.pytorch.org/whl/cu124
+uv pip install torch --link-mode=copy --extra-index-url https://download.pytorch.org/whl/cu124
 
 echo.
 echo [3/4] Installing wLLM and its dependencies...
-uv pip install -e . --extra-index-url https://download.pytorch.org/whl/cu124
+uv pip install -e . --link-mode=copy --extra-index-url https://download.pytorch.org/whl/cu124
 
 echo.
 echo [4/4] Adding wLLM to system PATH...
